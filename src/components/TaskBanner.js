768f20434b5fd6f6fd103e263b8c9b803c1cfb6d
wilmer <est.wzelayam@uml.edu.ni>
2023-07-29 10:51:15 -0600

    deploy

--- src/components/TaskBanner.js
+++ src/components/TaskBanner.js
@@ -1,9 +1,9 @@
 export const TaskBanner = ({ userName, taskItems }) => (
   <h4
     className="text-white text-center p-4 mb-5 fw-bold"
-    style={{ background: "#303030" }}
+    style={{ background: "blue" }}
   >
-    {userName}'s Tasks App{" "}
+    Lista de Tareas{" "}
     <span className="text-info">
       ({taskItems.filter((t) => !t.done).length} tasks to do)
     </span>
